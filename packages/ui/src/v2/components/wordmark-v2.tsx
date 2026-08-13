33f4e447b7daded78377d9e4e9bd5d70b6a76e8e
kel1y <irakozekelly41@gmail.com>
2026-08-13 09:22:33 -0400

    water gone

--- packages/ui/src/v2/components/wordmark-v2.tsx
+++ packages/ui/src/v2/components/wordmark-v2.tsx
@@ -14,46 +14,43 @@ export function WordmarkV2(props: Pick<ComponentProps<"svg">, "class">) {
       <g opacity="0.6">
         <g mask={`url(#${mask})`}>
           <g opacity="0.16">
-            <path
-              opacity="0.7"
-              d="M55.3846 36.4286H18.4615V91.7143H55.3846V36.4286ZM73.8462 110.143H0V18H73.8462V110.143Z"
-              fill="currentColor"
-            />
-            <path
-              opacity="0.7"
-              d="M110.462 91.7143H147.385V36.4286H110.462V91.7143ZM165.846 110.143H110.462V128.571H92V18H165.846V110.143Z"
-              fill="currentColor"
-            />
-            <path
-              opacity="0.7"
-              d="M258.846 73.2857H203.462V91.7143H258.846V110.143H185V18H258.846V73.2857ZM203.462 54.8571H240.385V36.4286H203.462V54.8571Z"
-              fill="currentColor"
-            />
-            <path
-              opacity="0.7"
-              d="M332.385 36.4286H295.462V110.143H277V18H332.385V36.4286ZM350.846 110.143H332.385V36.4286H350.846V110.143Z"
-              fill="currentColor"
-            />
-            <path
-              opacity="0.7"
-              d="M442.846 36.4286H387.462V91.7143H442.846V110.143H369V18H442.846V36.4286Z"
-              fill="currentColor"
-            />
-            <path
-              opacity="0.7"
-              d="M517.385 36.4286H480.462V91.7143H517.385V36.4286ZM535.846 110.143H462V18H535.846V110.143Z"
-              fill="currentColor"
-            />
-            <path
-              opacity="0.7"
-              d="M609.385 36.8571H572.462V92.1429H609.385V36.8571ZM627.846 110.571H554V18.4286H609.385V0H627.846V110.571Z"
-              fill="currentColor"
-            />
-            <path
-              opacity="0.7"
-              d="M664.462 36.4286V54.8571H701.385V36.4286H664.462ZM719.846 73.2857H664.462V91.7143H719.846V110.143H646V18H719.846V73.2857Z"
-              fill="currentColor"
-            />
+            <g transform="translate(64.6154 0)">
+              <path
+                opacity="0.7"
+                d="M55.3846 18H73.8462V36.4286H55.3846V18ZM55.3846 54.8571H73.8462V110.143H55.3846V54.8571Z"
+                fill="currentColor"
+              />
+              <path
+                opacity="0.7"
+                transform="translate(92.3077 0)"
+                d="M55.3846 36.4286H18.4615V110.143H0V18H55.3846V36.4286ZM73.8462 110.143H55.3846V36.4286H73.8462V110.143Z"
+                fill="currentColor"
+              />
+              <path
+                opacity="0.7"
+                transform="translate(184.6154 0)"
+                d="M73.8462 36.4286H18.4615V91.7143H73.8462V110.143H0V18H73.8462V36.4286Z"
+                fill="currentColor"
+              />
+              <path
+                opacity="0.7"
+                transform="translate(276.9231 0)"
+                d="M55.3846 36.4286H18.4615V91.7143H55.3846V36.4286ZM73.8462 110.143H0V18H73.8462V110.143Z"
+                fill="currentColor"
+              />
+              <path
+                opacity="0.7"
+                transform="translate(369.2308 0)"
+                d="M55.3846 36.8571H18.4615V92.1429H55.3846V36.8571ZM73.8462 110.571H0V18.4286H55.3846V0H73.8462V110.571Z"
+                fill="currentColor"
+              />
+              <path
+                opacity="0.7"
+                transform="translate(461.5385 0)"
+                d="M73.8462 73.2857H18.4615V91.7143H73.8462V110.143H0V18H73.8462V73.2857ZM18.4615 54.8571H55.3846V36.4286H18.4615V54.8571Z"
+                fill="currentColor"
+              />
+            </g>
           </g>
         </g>
       </g>
